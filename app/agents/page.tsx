007ca78270b4307aa99b799b3fce255cbbde0b1d
ANSH-1n <anshjamwal10102003@gmail.com>
2026-01-08 23:31:21 +0530

    Add the next version 15.3.7

--- app/agents/page.tsx
+++ app/agents/page.tsx
@@ -465,10 +465,10 @@ const AIAgentsLandingPage: React.FC = () => {
                         />
                       </svg>
                     </div>
-                    <div className="font-bold">Automated Activity</div>
+                    <div className="font-bold">Automated Activity.You can check here</div>
                   </div>
                   <p className="text-sm mb-4">
-                    Automated 0 out of 0 interactions.Check this
+                    Automated 0 out of 0 interactions
                   </p>
                 </div>
               </div>
